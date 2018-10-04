7bff31f8ccff0bd8ea3b64e003a376c9db85b2b4
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-04 16:09:08 -0400

    Add #first_anagram?

--- executions_time_diff/anagrams.rb
+++ executions_time_diff/anagrams.rb
@@ -1,3 +1,6 @@
 def first_anagram?(string1, string2)
-  
+  letters = string1.chars
+  perms = letters.permutation(string1.length).to_a #O(n!)
+  all_words = perms.map(&:join)
+  all_words.include?(string2)
 end
