eb73e276bb467c16adb7a480c00e1d5a5f2b2256
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-04 16:22:16 -0400

    Finish #second_anagram?

--- executions_time_diff/anagrams.rb
+++ executions_time_diff/anagrams.rb
@@ -4,3 +4,16 @@ def first_anagram?(string1, string2)
   all_words = perms.map(&:join)
   all_words.include?(string2)
 end
+
+def second_anagram?(string1, string2)
+  string2_chars = string2.chars
+  string1.each_char do |letter|
+    unless string2_chars.find_index(letter) == nil
+      is_found = string2_chars.find_index(letter)
+      string2_chars.delete_at(is_found)
+    else
+      return false
+    end
+  end
+  true
+end
